2d4c63c5c15ce3621518668861aa89cb4027ec30
michaelFawv <michaelFawv@users.noreply.github.com>
2015-11-14 16:54:44 -0800

    commit

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ app.set('port', 3000);
 app.use(express.static(__dirname));
 
 //Setup client server
-var httpapp = http.createServer(app).listen(app.get('port'));
+var httpapp = http.createServer(app).listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
  
 var io = require('socket.io').listen(httpapp);
 
